b05e4e450f2e297771b59232f589255681f2b7b8
Georg Ekeberg <georghagen@gmail.com>
2020-08-21 16:21:18 +0200

    done something

--- encourageQuotes.js
+++ encourageQuotes.js
@@ -1,3 +1,5 @@
-module.exports =  ["Every great journey starts with one small step", "If you’re going through hell, keep going. – Winston Churchill",
+module.exports =  
+["Every great journey starts with one small step", 
+"If you’re going through hell, keep going. – Winston Churchill",
 "What’s done is done. What’s gone is gone. One of life’s lessons is always moving on. It’s okay to look back to see how far you’ve come but keep moving forward. – Roy T. Bennett", "Courage is not having the strength to go on; it is going on when you don’t have the strength.- Theodore Roosevelt",
 "I always tell my kids if you lay down, people will step over you. But if you keep scrambling, if you keep going, someone will always, always give you a hand. Always. But you gotta keep dancing, you gotta keep your feet moving. – Morgan Freeman"]
